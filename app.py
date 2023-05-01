fad4f86361d5d4fe679d21fee6cf443b818dfac0
Ashley Lin <ashleyylin11@gmail.com>
2023-05-01 12:07:14 -0700

    add cupcake model and view functions to list, get, and create cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,58 @@
+from flask import Flask, jsonify, redirect, render_template, flash, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+# from forms import NewSongForPlaylistForm, AddSongForm, AddPlaylistForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+connect_db(app)
+
+app.config["SECRET_KEY"] = "I'LL NEVER TELL!!"
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
 """Flask app for Cupcakes"""
+
+
+@app.get("/api/cupcakes")
+def show_all_cupcakes():
+    """Return data about all the cupcakes."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def show_one_cupcake(cupcake_id):
+    """Return data about a single cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.post("/api/cupcakes")
+def create_one_cupcake():
+    """Return data about a single cupcake."""
+
+    data = {k: v for k, v in request.json.items()}
+    new_cupcake = Cupcake(**data)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
